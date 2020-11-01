2735a71c2e2925616907ef9f0d5b7722ca7470de
exylaci <exy@freemail.hu>
2020-11-01 12:41:08 +0100

    Calculator.

---
+++ src/main/java/classstructureio/Registration.java
@@ -0,0 +1,18 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("What is your e-mail address?");
+        String email = scanner.nextLine();
+
+        System.out.println("Hi " + name + "!");
+        System.out.println("You've successfully registered with " + email + " e-mail address.");
+    }
+}
