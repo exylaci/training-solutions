4fea80d5cbdb442dd79fba0c32a6e07ccc421262
exylaci <exy@freemail.hu>
2020-11-01 19:19:55 +0100

    Könyv osztály: Hozz létre egy Book osztályt, melynek három privát attribútuma van: szerző (author), cím (title) és regisztrációs szám (regNumber), mindhárom típusa String. A Book példányosításakor csak a szerzőt és a címet kelljen megadni. Legyen egy public void register(String regNumber) metódusa, mely a nyilvántartásba vételt implementálja, és ennek paraméterül kell megadni a regisztrációs számot. Írj egy main() metódust a BookMain osztályba, amivel kipróbálod a működését! Az attribútumok kiolvasásához használj gettereket!

---
+++ src/main/java/classsctructureconstructors/Book.java
@@ -0,0 +1,28 @@
+package classsctructureconstructors;
+
+public class Book {
+    private String author;
+    private String title;
+    private String regNumber;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public void register(String regNumber){
+        this.regNumber=regNumber;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+}
---
+++ src/main/java/classsctructureconstructors/BookMain.java
@@ -0,0 +1,10 @@
+package classsctructureconstructors;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Book book=new Book("Marék Veronika","Már tudok olvasni!");
+        book.register("HU123-1");
+
+        System.out.println(book.getAuthor() + ": " + book.getTitle() + " (" + book.getRegNumber() +")");
+    }
+}
