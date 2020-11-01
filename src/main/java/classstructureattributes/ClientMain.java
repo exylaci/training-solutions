4d66e2ed3706c9f31b9e7fef784790bf41d20496
exylaci <exy@freemail.hu>
2020-11-01 16:11:41 +0100

    Hozz létre egy Client osztályt, melynek három attribútuma van: név (name), születési év (year) és cím (address). Típusaik rendre String, int és String. Hozz létre egy main() metódust a ClientMain osztályba, amelyben kipróbálod a Client osztály működését. Példányosítani kell egy objektumot a Client osztály alapján, majd kérd be az attribútumok értékét a felhasználótól. Ellenőrzésképp írd ki minden attribútumának értékét a konzolra!

---
+++ src/main/java/classstructureattributes/ClientMain.java
@@ -0,0 +1,23 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        System.out.println("What is the name of the client?");
+        Scanner scanner = new Scanner(System.in);
+        client.name= scanner.nextLine();
+
+        System.out.println("What is the address of the client?");
+        client.address = scanner.nextLine();
+
+        System.out.println("When the client was born?");
+        client.year = scanner.nextInt();
+
+        System.out.println("Client name:" + client.name);
+        System.out.println("Client birth date:" + client.year);
+        System.out.println("Client address:" + client.address);
+    }
+}
