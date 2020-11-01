aecfbf475af089ddb44e37cf85207397e1cc6eb2
exylaci <exy@freemail.hu>
2020-11-01 20:03:46 +0100

    Termék: Az alábbi diagram és a leírás alapján készítsd el a Product osztályt! Az attribútumok kezdőértéket a konstruktorban kapnak. Az increasePrice() a paraméter értékével növeli, a decreasePrice() pedig csökkenti az aktuális árat. Próbáld ki az osztály működését main() metódusban, ahol a példány létrehozásához szükséges adatokat a felhasználótól kéred be!

---
+++ src/main/java/classsctructureintegrate/ProductMain.java
@@ -0,0 +1,20 @@
+package classsctructureintegrate;
+
+import java.util.Scanner;
+
+public class ProductMain {
+    public static void main(String[] args) {
+        System.out.println("What is the product?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("What does it costs?");
+        int price = scanner.nextInt();
+
+        Product product = new Product(name,price);
+        product.increasePrice(50);
+        System.out.println("New price: "+product.getPrice());
+        product.decreasePrice(100);
+        System.out.println("New price: "+product.getPrice());
+    }
+}
