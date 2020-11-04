9289305a745ad0a763652c3732ee4ac37a0a8a74
exylaci <exy@freemail.hu>
2020-11-04 23:06:29 +0100

    Bonyolultabb típusok / Tömbök / Keresés

---
+++ src/main/java/array/ArrayHandler.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class ArrayHandler {
+    boolean contains(int[] source, int itemToFind){
+        // return Arrays.stream(source).anyMatch(x -> x == itemToFind);
+
+        for (int oneItem: source) {
+            if (oneItem==itemToFind) return true;
+        }
+        return false;
+    }
+
+    int find(int[] source, int itemToFind){
+        for(int i=0; i<source.length;++i){
+            if(source[i]==itemToFind) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ArrayHandler a = new ArrayHandler();
+        int[] i = {1,2,3,4};
+        System.out.println( a.contains(i, 7) );
+        System.out.println( a.find(i, 7) );
+
+        System.out.println( a.contains(i, 3) );
+        System.out.println( a.find(i, 3) );
+    }
+}
