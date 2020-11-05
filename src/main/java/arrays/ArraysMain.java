403ddb849ee9cdc0ca426bc085deb4c9d76f7fb4
exylaci <exy@freemail.hu>
2020-11-05 23:35:21 +0100

    Bonyolultabb típusok / Tömbök kezelése / wonLottery

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -51,22 +52,49 @@ public class ArraysMain {
         return false;
     }
 
+    public boolean wonLottery(int[] bet, int[] winner){
+        int[] one = new int[bet.length];
+        one=bet.clone();
+        int[] two = new int[winner.length];
+        two=winner.clone();
+        Arrays.sort(one);
+        Arrays.sort(two);
+        // System.out.println(Arrays.toString( bet ));
+        // System.out.println(Arrays.toString( one));
+        // System.out.println(Arrays.toString( winner ));
+        // System.out.println(Arrays.toString( two));
+        return Arrays.equals(one,two);
+    }
+
     public static void main(String[] args) {
         ArraysMain a = new ArraysMain();
+        System.out.println("Hónapok hossza:");
         System.out.println(a.numberOfDaysAsString());
         System.out.println("");
 
+        System.out.println("Hét napjai:");
         System.out.println(a.daysOfWeek());
         System.out.println("");
 
+        System.out.println("Szorzótábla sztring:");
         System.out.println(a.multiplicationTableAsString(4));
         System.out.println();
 
+        System.out.println("Napi órás mérési adatok:");
         System.out.println(a.sameTempValues(
                 new double[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,17,18,19,20,21,22,23},
                 new double[] {31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53}));
         System.out.println(a.sameTempValues(
                 new double[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,17,18,19,20,21,22,23},
                 new double[] {31,32,33,22,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53}));
+        System.out.println();
+
+        System.out.println("Lottó ötös-e:");
+        System.out.println(a.wonLottery(
+                new int[] {12,65,23,45,29},
+                new int[] {43,76,86,89,1}));
+        System.out.println(a.wonLottery(
+                new int[] {12,65,23,45,29},
+                new int[] {45,29,23,12,65}));
     }
 }
