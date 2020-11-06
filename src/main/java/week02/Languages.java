ae0420674c8103ed59b0bfd545435f0bfb2e50a6
exylaci <exy@freemail.hu>
2020-11-06 16:58:33 +0100

    week02 / órai munka #3

--- src/main/java/week02/Languages.java
+++ src/main/java/week02/Languages.java
@@ -1,6 +1,5 @@
 package week02;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,7 +7,7 @@ public class Languages {
     public static void main(String[] args) {
         List<String> languages = Arrays.asList("Java","Python","JavaSript");
         for (String one: languages) {
-            if(one.length() > 5){
+            if( one.length() > 5 ){
                 System.out.println(one);
             }
         }
