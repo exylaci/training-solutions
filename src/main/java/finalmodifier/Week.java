ab0bb672f9e0cdd415830277585b6a3691824e1a
exylaci <exy@freemail.hu>
2020-11-08 23:54:50 +0100

    Bonyolultabb típusok / Konstans értékek használata / Bónusz feladat 2.

--- src/main/java/finalmodifier/Week.java
+++ src/main/java/finalmodifier/Week.java
@@ -7,8 +7,9 @@ import java.util.List;
 
 public class Week {
     public final List<String> daysOfWeek = Arrays.asList("Hétfő","Kedd","Szerda","Csütörtök","Péntek","Szombat","Vasárnap");
+    //public final int j;   // kell neki értéket adni.
 
-    public static void main(String[] args) {
+    public static <i> void main(String[] args) {
         Week week = new Week();
         System.out.println(week.daysOfWeek);
 
@@ -16,6 +17,12 @@ public class Week {
         System.out.println(week.daysOfWeek);
 
         List<String> otherList = Arrays.asList("Egy","Kettő");
-//        week.daysOfWeek = otherList;      // final-nak nem lehet újra értéket adni
+        // week.daysOfWeek = otherList;      // final-nak nem lehet újra értéket adni
+
+        System.out.println("\n\rBonusz 1.");
+        final int i;
+        i=1;        //valmikor (első felhasználás előtt) kell neki értéket adni
+        // i=2;     //csak egyszer lehet értéket adni
+        System.out.println(i);
     }
 }
