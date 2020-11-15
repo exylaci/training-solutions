e7cfadef52de3a0b35a9e579b3b6b00ff52137ff
exylaci <exy@freemail.hu>
2020-11-15 09:54:47 +0100

    String, StringBuilder / Főbb String metódusok / Fájlnevek kezelése (stringmethods.filename)

--- src/main/java/stringmethods/filename/FileNameManipulator.java
+++ src/main/java/stringmethods/filename/FileNameManipulator.java
@@ -3,44 +3,41 @@ package stringmethods.filename;
 public class FileNameManipulator {
     public char findLastCharacter(String str){
         isBlank(str,"Empty string!");
-
-        return ' ';
+        String trimmed = str.trim();
+        return trimmed.charAt( trimmed.length()-1 );
     }
     public String findFileExtension(String fileName){
         isBlank(fileName,"Invalid file name!");
-        if ( ".".equals(fileName) || ".d".equals(fileName) ){
-            throw  new IllegalArgumentException("Invalid file name!");
-        }
-
-        return "";
+        isNoFileName(fileName,"Invalid file name!");
+        String trimmed = fileName.trim();
+        return trimmed.substring( trimmed.lastIndexOf('.') );
     }
 
     public boolean identifyFilesByExtension(String ext, String fileName){
-        if ( "d".equals(ext) || ".d".equals(fileName) ){
-            throw  new IllegalArgumentException("Invalid argument!");
-        }
-
-        return false;
+        isBlank(ext,"Invalid argument!");
+        isBlank(fileName,"Invalid argument!");
+        isNoFileName(fileName,"Invalid argument!");
+        return ext.trim().equals( findFileExtension(fileName).substring(1) );
     }
 
     public boolean compareFilesByName(String searchedFileName, String actualFileName){
-
-        return false;
+        isBlank(searchedFileName,"Invalid argument!");
+        isBlank(actualFileName,"Invalid argument!");
+        return searchedFileName.toLowerCase().equals( actualFileName.toLowerCase() );
     }
 
     public String changeExtensionToLowerCase(String fileName){
         isBlank(fileName,"Empty string!");
-        if (".".equals(fileName) || ".java".equals(fileName)){
-            throw  new IllegalArgumentException("Invalid argument!");
-        }
+        String trimmed = fileName.trim();
+        isNoFileName(trimmed,"Invalid argument!");
+        int pos = trimmed.lastIndexOf('.');
 
-
-        return "";
+        return trimmed.substring(0,pos) + trimmed.substring(pos).toLowerCase();
     }
 
     public String replaceStringPart(String fileName, String present, String target){
-
-        return "";
+        isBlank(fileName,"Empty string!");
+        return fileName.replace(present,target);
     }
 
     private void isBlank(String check,String errorMessage){
@@ -48,4 +45,12 @@ public class FileNameManipulator {
             throw  new IllegalArgumentException(errorMessage);
         }
     }
+
+    private void isNoFileName(String check,String message){
+        if ( check.charAt(0) == '.' ){
+            throw new IllegalArgumentException(message);
+        }
+    }
 }
+
+
