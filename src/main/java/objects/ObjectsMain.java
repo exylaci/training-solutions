19d6ec5405203d70e4360cf076b8473044a0bc97
exylaci <exy@freemail.hu>
2020-11-22 23:08:03 +0100

    Bevezetés az osztályok és objektumok világába / Objektumok / Objektumok száma (objects)

--- src/main/java/objects/ObjectsMain.java
+++ src/main/java/objects/ObjectsMain.java
@@ -23,5 +23,17 @@ public class ObjectsMain {
         System.out.println(book==anotherBook);
 
         System.out.println(anotherBook instanceof Book);
+
+
+        Book book1 = new Book();        // 1 db
+        Book book2 = new Book();        // 2 db
+        Book book3 = new Book();        // 3 db
+        Book book4 = book1;             // 3 db
+        Book book5 = book1;             // 3 db
+        Book book6 = book3;             // 3 db
+        Book book7 = null;              // 3 db
+        book4 = book5;                  // 3 db
+        book5 = new Book();             // 4 db
+        book6 = null;                   // 4 db
     }
 }
