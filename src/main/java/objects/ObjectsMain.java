e4b97374ad6ec17ded9fc884de1ce5b69c7528e8
exylaci <exy@freemail.hu>
2020-11-22 23:13:32 +0100

    Bevezetés az osztályok és objektumok világába / Objektumok / Tömbök és kollekciók (objects)

--- src/main/java/objects/ObjectsMain.java
+++ src/main/java/objects/ObjectsMain.java
@@ -1,7 +1,13 @@
 package objects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ObjectsMain {
     public static void main(String[] args) {
+
+        //Objektumok
         new Book();                             // lehet példányosítani, de nem lehet elérni
 
         Book book = new Book();
@@ -25,6 +31,7 @@ public class ObjectsMain {
         System.out.println(anotherBook instanceof Book);
 
 
+        //Objektumok száma
         Book book1 = new Book();        // 1 db
         Book book2 = new Book();        // 2 db
         Book book3 = new Book();        // 3 db
@@ -35,5 +42,15 @@ public class ObjectsMain {
         book4 = book5;                  // 3 db
         book5 = new Book();             // 4 db
         book6 = null;                   // 4 db
+
+        //Tömbök és kollekciók
+        Book[] books = {book1,book2,book3};
+
+        List<Book> books2 = Arrays.asList(book1,book2,book3);
+
+        List<Book> emptyBooks = new ArrayList<>();
+        emptyBooks.add(book1);
+        emptyBooks.add(book2);
+        emptyBooks.add(book3);
     }
 }
