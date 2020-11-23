8d872aea394f42ada03ab793b08b7b63c7db2e28
exylaci <exy@freemail.hu>
2020-11-23 11:32:33 +0100

    Típusok és operátorok / Operátorok / Bónusz1 feladat (operators)

--- src/main/java/operators/OperatorsMain.java
+++ src/main/java/operators/OperatorsMain.java
@@ -15,5 +15,13 @@ public class OperatorsMain {
         System.out.println( i >> 1);
 
         System.out.println(o.multiplyByPowerOfTwo(5,3));  //5*2^3
+
+
+        i = -1;
+        String s = Integer.toBinaryString(i);   // kettes komplemenst csinl
+        System.out.println(s);
+//        int j = Integer.parseInt(s, 2);       // "nem tudja", hogy ez egy kettes komplemensben ábrázolt negatív szám.
+        long j = Long.valueOf(s, 2);
+        System.out.println(j);
     }
 }
