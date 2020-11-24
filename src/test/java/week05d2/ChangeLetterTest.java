13bc4f0d16ed3d3292d2c03f31287008266e65c7
exylaci <exy@freemail.hu>
2020-11-24 20:00:37 +0100

    week 05 / kedd / órai munka (week05d2)

---
+++ src/test/java/week05d2/ChangeLetterTest.java
@@ -0,0 +1,16 @@
+package week05d2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChangeLetterTest {
+    @Test
+    void ChangeLetterTest() {
+        
+        assertEquals("*bcd*fgh*jklmn*pqrst*vwxyz", new ChangeLetter().chhangeVowels("abcdefghijklmnopqrstuvwxyz"));
+        assertEquals("", new ChangeLetter().chhangeVowels(null));
+        assertEquals("", new ChangeLetter().chhangeVowels(""));
+        assertEquals("*", new ChangeLetter().chhangeVowels("a"));
+    }
+}
