c4b3456a1328223499d2be4f9e3c8d32f4d43f72
exylaci <exy@freemail.hu>
2020-11-25 23:32:48 +0100

    Attribútumok és metódusok / Metódusnév túlterhelés / Time, Pub, Bónusszal (methodoverloading)

---
+++ src/main/java/methodoverloading/time/Time.java
@@ -0,0 +1,69 @@
+package methodoverloading.time;
+
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time(int hours, int minutes, int seconds) {
+        this(hours, minutes);
+        this.seconds = seconds;
+    }
+
+    public Time(int hours, int minutes) {
+        this(hours);
+        this.minutes = minutes;
+    }
+
+    public Time(int hours) {
+        this.hours = hours;
+    }
+
+    public Time(Time time) {
+        this(time.getHours(), time.getMinutes(), time.getSeconds());
+    }
+
+    public boolean isEqual(Time time) {
+        return isEqual(time.getHours(), time.getMinutes(), time.getSeconds());
+    }
+
+    public boolean isEqual(int hours, int minutes, int seconds) {
+        if (this.hours == hours &&
+                this.minutes == minutes &&
+                this.seconds == seconds) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isEarlier(Time time) {
+        return isEarlier(time.getHours(), time.getMinutes(), time.getSeconds());
+    }
+
+    public boolean isEarlier(int hours, int minutes, int seconds) {
+        if (this.hours < hours) {
+            return true;
+        } else if (this.hours > hours) {
+            return false;
+        } else if (this.minutes < minutes) {
+            return true;
+        } else if (this.minutes > minutes) {
+            return false;
+        } else if (this.seconds < seconds) {
+            return true;
+        }
+        return false;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+}
---
+++ src/test/java/methodoverloading/time/TimeTest.java
@@ -0,0 +1,20 @@
+package methodoverloading.time;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TimeTest {
+    @Test
+    public void timeTest() {
+        Time t1 = new Time(1);
+        Time t2 = new Time(1, 2);
+        Time t3 = new Time(1, 2, 3);
+        Time t4 = new Time(t3);
+
+        assertTrue(t1.isEarlier(t2));
+        assertTrue(t3.isEarlier(1, 2, 4));
+        assertFalse(t3.isEarlier(1, 2, 3));
+    }
+}
