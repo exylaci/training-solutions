4039ebbe9377b90e84a6f19f002ac3ea2e43c847
exylaci <exy@freemail.hu>
2020-11-25 20:58:58 +0100

    Attribútumok és metódusok / Változó hosszúságú paraméterlista / Vizsga statisztika (methodargvars.examstats)

---
+++ src/test/java/methodargvars/trainer/TrainerTest.java
@@ -0,0 +1,19 @@
+package methodargvars.trainer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrainerTest {
+
+    @Test
+    void addCoursesTest(){
+        Trainer t = new Trainer();
+        t.addCourses("C#","java");
+        assertEquals(2,t.getCourses().size() );
+
+        String[] names = {"C++","pascal"};
+        t.addCourses("python", names);
+        assertEquals(5,t.getCourses().size() );
+    }
+}
