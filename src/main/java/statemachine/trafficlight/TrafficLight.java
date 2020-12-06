3b126d086d0adeb2a07dcb40635d02ee2fb77aee
exylaci <exy@freemail.hu>
2020-12-06 01:04:31 +0100

    Absztrakt osztályok és interfészek / Állapotgép típusokban / Közlekedési lámpa (statemachine.trafficlight)

---
+++ src/main/java/statemachine/trafficlight/TrafficLight.java
@@ -0,0 +1,33 @@
+package statemachine.trafficlight;
+
+public enum TrafficLight {
+    RED {
+        @Override
+        TrafficLight next() {
+            return RED_YELLOW;
+        }
+    },
+
+    RED_YELLOW {
+        @Override
+        TrafficLight next() {
+            return GREEN;
+        }
+    },
+
+    GREEN {
+        @Override
+        TrafficLight next() {
+            return YELLOW;
+        }
+    },
+
+    YELLOW {
+        @Override
+        TrafficLight next() {
+            return RED;
+        }
+    };
+
+    abstract TrafficLight next();
+}
