28075044eddfad8252b3d76fee1cb4ec979d72dd
exylaci <exy@freemail.hu>
2020-12-12 20:24:34 +0100

    Fájlkezelés / String írása Writerrel / Nevek (names)
    Az első feladatban egyszerűen neveket fogunk eltárolni fájlban és listában egyaránt. A NameWriter osztály konstruktorban megkapja az írni kívánt fájlt. Az addAndWrite() metódus egy nevet vár paraméterül, amelyet hozzáfűzi a fájlhoz. A hozzáfűzéshez használd a korábban megismert StandardOpenOption.Append paramétert a newBufferedWriter() metódusban.

---
+++ src/main/java/names/NameWriter.java
@@ -0,0 +1,31 @@
+package names;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class NameWriter {
+    private Path path;
+
+    public NameWriter(Path path) {
+        this.path = path;
+    }
+
+    public void addAndWrite(String name) {
+        try {
+            BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+            writer.write(name);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Can't write to file", e);
+        }
+    }
+}
+//Az első feladatban egyszerűen neveket fogunk eltárolni fájlban és listában egyaránt.
+// A NameWriter osztály konstruktorban megkapja az írni kívánt fájlt.
+// Az addAndWrite() metódus egy nevet vár paraméterül, amelyet hozzáfűzi a fájlhoz.
+// A hozzáfűzéshez használd a korábban megismert StandardOpenOption.Append paramétert a newBufferedWriter() metódusban.
