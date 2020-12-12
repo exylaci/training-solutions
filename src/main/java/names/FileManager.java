b5b0a27cb92645d84242cce45ccf0ad0ca63576e
exylaci <exy@freemail.hu>
2020-12-12 11:36:12 +0100

    Fájlkezelés / Szöveges állomány beolvasása / Emberek (names) A feladat egy szöveges állományból nevek beolvasása és eltárolása egy listába. A megoldáshoz két osztály kell megvalósítanod. A Human osztály reprezentál egy embert. Két adattagja vezeték- illetve keresztnév. A FileManager osztály felelős a fájl feldolgozásért. Egy Path típusú attribútumon keresztül érjük el a fájlt, míg a readFromFile() metódus felelős a beolvasásért és a Human objektumok létrehozásáért. Szöveges állomány: src/test/resources/names.txt

---
+++ src/main/java/names/FileManager.java
@@ -0,0 +1,40 @@
+package names;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager  {
+    private Path path;
+    private List<Human> names = new ArrayList<>();
+
+    public FileManager(String path) {
+        this.path = Path.of(path);
+    }
+
+    public void readFromFile(){
+        try {
+            List<String> lines= Files.readAllLines(path, StandardCharsets.UTF_8);
+            for (String one : lines) {
+                names.add(new Human(one));
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Can't read from file!");
+        }
+    }
+
+    public Path getMyFile() {
+        return path;
+    }
+
+    public List<Human> getHumans() {
+        return names;
+    }
+}
+//A FileManager osztály felelős a fájl feldolgozásért.
+// Egy Path típusú attribútumon keresztül érjük el a fájlt, míg a
+// readFromFile() metódus felelős a beolvasásért és a
+// Human objektumok létrehozásáért.
---
+++ src/main/java/names/Human.java
@@ -0,0 +1,29 @@
+package names;
+
+public class Human {
+    private String lastName;
+    private String firstName;
+
+    public Human(String firstName, String lastName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public Human(String fullName) {
+        if (fullName != null) {
+            String[] split = fullName.split(" ");
+            this.firstName = split[0];
+            if (split.length > 0) {
+                this.lastName = split[1];
+            }
+        }
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
