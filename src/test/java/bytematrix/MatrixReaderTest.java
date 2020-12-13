f67a048426b977fea268cc9e3124122cf01eaf2e
exylaci <exy@freemail.hu>
2020-12-13 20:25:06 +0100

    Fájlkezelés / Bájtok olvasása InputStreammel / Mátrix (bytematrix)
    Ebben a feladatban egy mátrix adatszerkezettel kell dolgoznod. Hozz létre egy listát, ami byte tömbök tárolására alkalmas! Tárold el ebben a listában a mátrix.dat állományból beolvasott, minden 1000 bájtot tartalmazó byte tömböt!
    
    Az állományban csak egyesek és nullák vannak. Írj egy metódust, ami visszaadja azon oszlopok számát, ahol több nulla, mint egyes van a mátrixban!

---
+++ src/test/java/bytematrix/MatrixReaderTest.java
@@ -0,0 +1,33 @@
+package bytematrix;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MatrixReaderTest {
+    MatrixReader matrixReader = new MatrixReader();
+
+    @Test
+    public void createMatrixTest(){
+        matrixReader.createMatrix("matrix.dat");
+    }
+
+
+    @Test
+    public void readBytesAndCreateMatrixTest() {
+        assertEquals(0, matrixReader.getMyMatrix().size());
+        matrixReader.readBytesAndCreateMatrix("matrix.dat");
+
+        assertEquals(9, matrixReader.getMyMatrix().size());
+        assertEquals(1000, matrixReader.getMyMatrix().get(5).length);
+    }
+
+
+    @Test
+    public void numberOfColumnsWhereMoreZeros() {
+        matrixReader.readBytesAndCreateMatrix("matrix.dat");
+        assertEquals(493, matrixReader.numberOfColumnsWhereMoreZeros());
+    }
+
+}
