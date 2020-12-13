5cdeab7bcec38b01bf664f5312bd74ba2e543171
exylaci <exy@freemail.hu>
2020-12-13 22:27:38 +0100

    Fájlkezelés / String olvasása classpathról / Országok (countries)
    A src/main/resources/country.txt állományban országnevek és a szomszédos országok száma található. Hozz létre egy az ország tárolására alkalmas osztályt Country néven!
    
    Hozz létre egy CountryStatistics osztályt, ahol beolvasod file tartalmát egy listába, amjd írj metódusokat amelyek visszatérési értéke választ ad a következő kérdésekre: * Hány országot olvastál be? * Melyik országnak van a legtöbb szomszédja?

---
+++ src/main/java/countries/Country.java
@@ -0,0 +1,19 @@
+package countries;
+
+public class Country {
+    private String name;
+    private int borderCountries;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBorderCountries() {
+        return borderCountries;
+    }
+
+    public Country(String name, int borderCountries) {
+        this.name = name;
+        this.borderCountries = borderCountries;
+    }
+}
