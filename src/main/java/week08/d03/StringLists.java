843baa0bd51f8b46201290263ff5eafea80cb440
exylaci <exy@freemail.hu>
2020-12-16 09:07:18 +0100

    week 08 / szerda / órai feladat: union of strings, shortest strings
    1.
    Készíts week08d03 csomagban egy StringLists osztályt és benne egy stringListsUnion(List<String> first, List<String> second) metódust, mely két string listát vár paraméterül és visszaadja az uniójukat. Az úniónak minden elemet tartalmaznia kell mindkét listából, de egy elem nem szerepelhet kétszer!
    
    2.
    Készíts a week08d03 csomagban egy StringLists osztályt és benne egy shortestWords(List<String> words) metódust, mely paraméterül kap egy String listát és a legrövidebb szavakat vissza adja belőle egy Listában. PL :{"aaa", "aa", "bb", "cccc", "dd"} Itt a legrövidebb szavak az "aa", "bb", "dd".

--- src/main/java/week08/d03/StringLists.java
+++ src/main/java/week08/d03/StringLists.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 public class StringLists {
     public static List<String> shortestWords(List<String> words) {
+        if (words == null) return null;
+
         List<String> result = new ArrayList<>();
         int shortest = Integer.MAX_VALUE;
+
         for (String one : words) {
             if (one.length() < shortest) {
                 result.clear();
@@ -17,6 +20,20 @@ public class StringLists {
             }
         }
         return result;
+    }
+
+    public static List<String> stringListsUnion(List<String> first, List<String> second) {
+        if (first == null || second == null) return null;
+
+        List<String> union = new ArrayList<>();
+
+        for (String one : first) {
+            if (second.contains(one) && !union.contains(one)) {
+                union.add(one);
+            }
+        }
+
+        return union;
     }
 }
 //Készíts a week08d03 csomagban egy StringLists osztályt és benne egy
--- src/test/java/week08/d03/StringListsTest.java
+++ src/test/java/week08/d03/StringListsTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringListsTest {
     @Test
@@ -15,7 +16,27 @@ public class StringListsTest {
 
         assertEquals(sample.size(), result.size());
         for (int i = 0; i < sample.size(); ++i) {
-            assertEquals(sample.get(i), result.get(i));
+            assertTrue(sample.get(i).equals(result.get(i)));
+        }
+    }
+
+    @Test
+    void shortestWordsNullTest() {
+        assertEquals(null, StringLists.shortestWords(null));
+    }
+
+
+    @Test
+    void stringListsUnionTest() {
+        List<String> sample = List.of("aa", "bb", "dd");
+        List<String> first = List.of("aa", "aa", "hh", "bb", "cc", "dd");
+        List<String> second = List.of("ss", "aa", "dd", "kk", "bb");
+        List<String> result = StringLists.stringListsUnion(first, second);
+
+
+        assertEquals(3, result.size());
+        for (String one : sample) {
+            assertTrue(result.contains(one));
         }
     }
 }
