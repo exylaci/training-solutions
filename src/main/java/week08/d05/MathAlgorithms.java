4df1294fa23f83030830a3088ae0155255444a96
exylaci <exy@freemail.hu>
2020-12-18 16:20:10 +0100

    week 08 / péntek / órai feladat: Legnagyobb közös osztó update: primtényezős felbontással :)
    A week08d05 csomagban készíts egy osztályt MathAlgorithms néven. Legyen benne egy metódus, greatestCommonDivisor() , ami paraméterül vár két pozitív egész számot és visszaadja a legnagyobb közös osztójukat.

--- src/main/java/week08/d05/MathAlgorithms.java
+++ src/main/java/week08/d05/MathAlgorithms.java
@@ -1,6 +1,18 @@
 package week08.d05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MathAlgorithms {
+    public static int legnagyobbKozosOsztoPrimTenyezokkel(int one, int two) {
+        List<Integer> ones = getPrims(one);
+        List<Integer> twos = getPrims(two);
+        List<Integer> crossSection = getCrossSection(ones, twos);
+        int result = multiply(crossSection);
+        return result;
+    }
+
+
     public static int greatestCommonDivisor(int one, int two) {
         if (one < 1 || two < 1) throw new IllegalArgumentException("Only positive numbers are accepted!");
         while (one != two) {
@@ -11,5 +23,52 @@ public class MathAlgorithms {
             }
         }
         return one;
+    }
+
+    private static List<Integer> getPrims(int value) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 2; i <= value; ++i) {
+            if (isPrim(i)) {
+                while (value % i == 0) {
+                    result.add(i);
+                    value /= i;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static boolean isPrim(int value) {
+        for (int i = 2; i <= Math.sqrt(value); ++i) {
+            if (value % i == 0) return false;
+        }
+        return true;
+    }
+
+    private static List<Integer> getCrossSection(List<Integer> ones, List<Integer> twos) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer one : ones) {
+            for (int i = 0; i < twos.size(); ++i) {
+                if (one == twos.get(i)) {
+                    result.add(one);
+                    twos.remove(i);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static int multiply(List<Integer> values) {
+        int result = 1;
+        for (Integer one : values) {
+            result *= one;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(legnagyobbKozosOsztoPrimTenyezokkel(20,30));
+        System.out.println(legnagyobbKozosOsztoPrimTenyezokkel(504,540));
     }
 }
