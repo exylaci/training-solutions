748a9af085e21c92fb8916b0e4213fa6b75bd703
exylaci <exy@freemail.hu>
2021-01-08 16:50:07 +0100

    week 10 / day 5 / órai feladat - Senior, Junior&MidLevel / MaxMin és Min keresés (week10.d05) UpDate: Visszatérési érték nélküli metódusok (Csak a képernyőre ír.) tesztelésével
    Mókázásból a forrás tömb növekvő sorba rendezése nélkül.

--- src/main/java/week10/d05/Calculator.java
+++ src/main/java/week10/d05/Calculator.java
@@ -5,25 +5,52 @@ import java.util.*;
 public class Calculator {
     public static int MAX_NUMBER_TO_SUMMARISE = 4;
 
+    public static void findMinMaxSum(int[] arr) {
+        if (arr == null) throw new IllegalArgumentException("Nem kaptam adatot!");
+
+        Arrays.sort(arr);
+
+        int min = 0;
+        int max = 0;
+        for (int i = 0; i < MAX_NUMBER_TO_SUMMARISE && i < arr.length; ++i) {
+            min += arr[i];
+            max += arr[arr.length - 1 - i];
+        }
+
+        System.out.printf("A legkissebbek összege: %d%nA legnagyobbak összege: %d%n", min, max);
+    }
+
     public static void findMinSum(int[] arr) {
         if (arr == null) throw new IllegalArgumentException("Nincs megadott adat!");
 
         int[] min = new int[MAX_NUMBER_TO_SUMMARISE];
-        for (int i=0;i<Math.min( MAX_NUMBER_TO_SUMMARISE, arr.length);++i){
-            min[i]=arr[i];
+        for (int i = 0; i < Math.min(MAX_NUMBER_TO_SUMMARISE, arr.length); ++i) {
+            min[i] = arr[i];
+        }
+        if (arr.length > MAX_NUMBER_TO_SUMMARISE) {
+            Arrays.sort(min);
         }
 
         for (int indexInArr = MAX_NUMBER_TO_SUMMARISE; indexInArr < arr.length; ++indexInArr) {
-            for (int indexInMin = 0; indexInMin < MAX_NUMBER_TO_SUMMARISE; ++indexInMin) {
+            for (int indexInMin = MAX_NUMBER_TO_SUMMARISE - 1; indexInMin >= 0; --indexInMin) {
                 if (arr[indexInArr] < min[indexInMin]) {
-                    min[indexInMin] = arr[indexInArr];
+
+                    for (int i=MAX_NUMBER_TO_SUMMARISE-1 ; i>0 ; --i){
+                        if ( min[i-1]<arr[indexInArr]){
+                            min[i]=min[i-1];
+                        }else{
+                            min[i]=arr[indexInArr];
+                            break;
+                        }
+                    }
+
                     break;
                 }
             }
         }
 
         int result = 0;
-        for (int i=0; i< Math.min(MAX_NUMBER_TO_SUMMARISE,arr.length);++i) {
+        for (int i = 0; i < Math.min(MAX_NUMBER_TO_SUMMARISE, arr.length); ++i) {
             result += min[i];
         }
 
@@ -49,5 +76,14 @@ public class Calculator {
         }
         findMinSum(data);
     }
-
 }
+//Készítsünk egy Calculator nevű osztályt, melynek van egy findMinSum(int[] arr) metódusa. A metódus feladata, hogy
+// kiírja a legkisebb összegeket 4 számból, amiket lehetséges összerakni az arr tömb elemeiből. Példa: ha az arr
+// tartalma [1, 3, 5, 7, 9], akkor a minimum összeg 1+3+5+7=16.
+//
+//
+//Készítsünk egy Calculator nevű osztályt, melynek van egy findMinMaxSum(int[] arr) metódusa. A metódus feladata, hogy
+// kiírja a legnagyobb és a legkisebb összegeket 4 számból, amiket lehetséges összerakni az arr tömb elemeiből. Példa:
+// ha az arr tartalma [1, 3, 5, 7, 9], akkor a minimum összeg 1+3+5+7=16 , a maximum összeg 3+5+7+9=24.A Calculator
+// osztály tartalmazzon egy main metódust is, amely a konzolról olvassa be a számokat és azokból készít tömböt, amivel
+// meghívja a findMinMaxSum metódust.
--- src/test/java/week10/d05/CalculatorTest.java
+++ src/test/java/week10/d05/CalculatorTest.java
@@ -2,27 +2,94 @@ package week10.d05;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static week10.d05.Calculator.findMinMaxSum;
+import static week10.d05.Calculator.findMinSum;
 
 public class CalculatorTest {
     @Test
     void findMinMaxSumTest() {
-        Calculator c = new Calculator();
 
-        assertEquals(0, c.findMinMaxSum(null).getMin());
-        assertEquals(4, c.findMinMaxSum(new int[]{4}).getMin());
-        assertEquals(-4, c.findMinMaxSum(new int[]{-4}).getMin());
-        assertEquals(10, c.findMinMaxSum(new int[]{1,2,3,4}).getMin());
-        assertEquals(24, c.findMinMaxSum(new int[]{8,9,2,5,13,222}).getMin());
-        assertEquals(-10, c.findMinMaxSum(new int[]{-1,-2,-3,-4,}).getMin());
-        assertEquals(-207, c.findMinMaxSum(new int[]{8,9,2,5,13,-222}).getMin());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream psold = System.out;
+        PrintStream psnew = new PrintStream(baos);
+        System.setOut(psnew);
 
-        assertEquals(0, c.findMinMaxSum(null).getMax());
-        assertEquals(4, c.findMinMaxSum(new int[]{4}).getMax());
-        assertEquals(-4, c.findMinMaxSum(new int[]{-4}).getMax());
-        assertEquals(10, c.findMinMaxSum(new int[]{1,2,3,4}).getMax());
-        assertEquals(252, c.findMinMaxSum(new int[]{8,9,2,5,13,222}).getMax());
-        assertEquals(-10, c.findMinMaxSum(new int[]{-1,-2,-3,-4,}).getMax());
-        assertEquals(35, c.findMinMaxSum(new int[]{8,9,2,5,13,-222}).getMax());
+        findMinMaxSum(new int[]{});
+        findMinMaxSum(new int[]{4});
+        findMinMaxSum(new int[]{-4});
+        findMinMaxSum(new int[]{1, 2, 3, 4});
+        findMinMaxSum(new int[]{8, 9, 2, 5, 13, 222});
+        findMinMaxSum(new int[]{-1, -2, -3, -4});
+        findMinMaxSum(new int[]{8, 9, 2, 5, 13, -222});
+
+        System.out.flush();
+        System.setOut(psold);
+
+        Scanner scanner = new Scanner(baos.toString());
+        assertEquals(String.format("A legkissebbek összege: 0"), scanner.nextLine());
+        assertEquals(String.format("A legnagyobbak összege: 0"), scanner.nextLine());
+        assertEquals(String.format("A legkissebbek összege: 4"), scanner.nextLine());
+        assertEquals(String.format("A legnagyobbak összege: 4"), scanner.nextLine());
+        assertEquals(String.format("A legkissebbek összege: -4"), scanner.nextLine());
+        assertEquals(String.format("A legnagyobbak összege: -4"), scanner.nextLine());
+        assertEquals(String.format("A legkissebbek összege: 10"), scanner.nextLine());
+        assertEquals(String.format("A legnagyobbak összege: 10"), scanner.nextLine());
+        assertEquals(String.format("A legkissebbek összege: 24"), scanner.nextLine());
+        assertEquals(String.format("A legnagyobbak összege: 252"), scanner.nextLine());
+        assertEquals(String.format("A legkissebbek összege: -10"), scanner.nextLine());
+        assertEquals(String.format("A legnagyobbak összege: -10"), scanner.nextLine());
+        assertEquals(String.format("A legkissebbek összege: -207"), scanner.nextLine());
+        assertEquals(String.format("A legnagyobbak összege: 35"), scanner.nextLine());
+    }
+
+    @Test
+    void findMinMaxSumErrorTest() {
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                findMinMaxSum(null));
+        assertEquals("Nem kaptam adatot!", exception.getMessage());
+    }
+
+    @Test
+    void findMinSumTest() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream psold = System.out;
+        PrintStream psnew = new PrintStream(baos);
+        System.setOut(psnew);
+
+        findMinSum(new int[]{});
+        findMinSum(new int[]{4});
+        findMinSum(new int[]{-4});
+        findMinSum(new int[]{1, 2, 3, 4});
+        findMinSum(new int[]{8, 9, 2, 5, 13, 222});
+        findMinSum(new int[]{-1, -2, -3, -4});
+        findMinSum(new int[]{8, 9, 2, 5, 13, -222});
+
+        System.out.flush();
+        System.setOut(psold);
+
+        Scanner scanner = new Scanner(baos.toString());
+        assertEquals(String.format("A legkisebb számok összege: 0"), scanner.nextLine());
+        assertEquals(String.format("A legkisebb számok összege: 4"), scanner.nextLine());
+        assertEquals(String.format("A legkisebb számok összege: -4"), scanner.nextLine());
+        assertEquals(String.format("A legkisebb számok összege: 10"), scanner.nextLine());
+        assertEquals(String.format("A legkisebb számok összege: 24"), scanner.nextLine());
+        assertEquals(String.format("A legkisebb számok összege: -10"), scanner.nextLine());
+        assertEquals(String.format("A legkisebb számok összege: -207"), scanner.nextLine());
+    }
+
+    @Test
+    void findMinSumErrorTest() {
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                findMinSum(null));
+        assertEquals("Nincs megadott adat!", exception.getMessage());
     }
 }
