fc8f646985820b83008342714248e3616dc33f08
exylaci <exy@freemail.hu>
2021-01-20 22:32:17 +0100

    week 12 / szerda / órai feladat: legkisebb unique elem keresése UpDate: fájlból beolvasásra átalakítva UpDate: stream-mel
    Készítsd el a NumberStat osztályt melyben legyen egy metódus, ami paraméterül vár egy egész számokból álló listát. Add vissza azt a számot amelyik pontosan egyszer szerepel a listában. Ha több ilyen szám van akkor a kisebbet!
    
    pl: {1,1,5,3,4,5,6,5,6,4,1,6,5,4} A megoldás: 3

--- src/main/java/week12/d03/NumberStat.java
+++ src/main/java/week12/d03/NumberStat.java
@@ -1,23 +1,34 @@
 package week12.d03;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class NumberStat {
+    public static final String SPLITTER = ",";
 
-    public int findSmallestUnique(List<Integer> list) {
+    List<Integer> list;
+
+    public NumberStat(List<Integer> list) {
+        this.list = list;
+    }
+
+    public int findSmallestUnique() {
         if (list == null) throw new IllegalArgumentException("The list is a must!");
 
         Optional<Map.Entry<Integer, Long>> result = list
                 .stream()
-                .filter((a) -> a != null)
+                .filter(a -> a != null)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
                 .filter(a -> a.getValue() == 1)
-                .min((a, b) -> (int) (a.getValue() - b.getValue()));
+                .min((a, b) -> (int) (a.getKey() - b.getKey()));
 
         if (result.isPresent()) {
             return result.get().getKey();
@@ -25,6 +36,43 @@ public class NumberStat {
 
         throw new IllegalStateException("There is no unique element in this list.");
     }
+
+    public void loadFromFile(Path pathAndFileName) {
+        if (pathAndFileName == null) throw new IllegalArgumentException("The file name is a must!");
+
+        try (BufferedReader reader = Files.newBufferedReader(pathAndFileName)) {
+            list = new ArrayList<>();
+            String oneLine;
+            while ((oneLine = reader.readLine()) != null) {
+                process(oneLine);
+            }
+
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't read from file: " + pathAndFileName);
+        }
+
+    }
+
+    private void process(String oneLine) {
+        String[] parts = oneLine.split(SPLITTER);
+        for (String oneData : parts) {
+            addOneData(oneData);
+        }
+    }
+
+    private void addOneData(String oneData) {
+        try {
+            list.add(Integer.parseInt(oneData));
+        } catch (NumberFormatException e) {
+//            System.out.println("This data isn't a number in the file: "+oneData);
+        }
+
+    }
+
+    public List<Integer> getList() {
+        return list;
+    }
+
 }
 //Készítsd el a week12d03.NumberStat osztályt melyben legyen egy metódus, ami paraméterül vár egy egész számokból álló
 // listát. Add vissza azt a számot amelyik pontosan egyszer szerepel a listában. Ha több ilyen szám van akkor a kisebbet!
--- src/test/java/week12/d03/NumberStatTest.java
+++ src/test/java/week12/d03/NumberStatTest.java
@@ -2,6 +2,8 @@ package week12.d03;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,56 +11,91 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NumberStatTest {
     @Test
-    void findSmallestUniqueTest() {
-        NumberStat n = new NumberStat();
+    void findSmallestUniqueTest1() {
+        NumberStat n = new NumberStat(List.of(1, 1, 1, 5, 3, 5, 6, 5, 5, 6, 1, 6, 5, 4));
+        assertEquals(3, n.findSmallestUnique());
+    }
 
-        assertEquals(3, n.findSmallestUnique(List.of(1, 1, 1, 5, 3, 5, 6, 5, 5, 6, 1, 6, 5, 4)));
-        assertEquals(1, n.findSmallestUnique(List.of(1)));
-        assertEquals(1, n.findSmallestUnique(List.of(2, 1)));
+    @Test
+    void findSmallestUniqueTest2() {
+        NumberStat n = new NumberStat(List.of(1));
+        assertEquals(1, n.findSmallestUnique());
+    }
+
+    @Test
+    void findSmallestUniqueTest3() {
+        NumberStat n = new NumberStat(List.of(2, 1));
+        assertEquals(1, n.findSmallestUnique());
     }
 
     @Test
     void findSmallestUniqueNullInListTest() {
-        NumberStat n = new NumberStat();
         List<Integer> temp = new ArrayList<>();
         temp.add(1);
         temp.add(null);
         temp.add(2);
+        NumberStat n = new NumberStat(temp);
 
-        assertEquals(1, n.findSmallestUnique(temp));
+        assertEquals(1, n.findSmallestUnique());
     }
 
     @Test
     void findSmallestUniqueNoUniqueTest() {
-        NumberStat n = new NumberStat();
+        NumberStat n = new NumberStat(List.of(1, 1, 2, 2));
 
-        Exception exception = assertThrows(IllegalStateException.class, () -> n.findSmallestUnique(List.of(1, 1, 2, 2)));
+        Exception exception = assertThrows(IllegalStateException.class, n::findSmallestUnique);
         assertEquals("There is no unique element in this list.", exception.getMessage());
     }
 
     @Test
     void findSmallestUniqueEmptyListTest() {
-        NumberStat n = new NumberStat();
+        NumberStat n = new NumberStat(new ArrayList<>());
 
-        Exception exception = assertThrows(IllegalStateException.class, () -> n.findSmallestUnique(new ArrayList<Integer>()));
+        Exception exception = assertThrows(IllegalStateException.class, n::findSmallestUnique);
         assertEquals("There is no unique element in this list.", exception.getMessage());
     }
 
     @Test
     void findSmallestUniqueOnlyNullInListParameterTest() {
-        NumberStat n = new NumberStat();
         List<Integer> temp = new ArrayList<>();
         temp.add(null);
+        NumberStat n = new NumberStat(temp);
 
-        Exception exception = assertThrows(IllegalStateException.class, () -> n.findSmallestUnique(temp));
+        Exception exception = assertThrows(IllegalStateException.class, n::findSmallestUnique);
         assertEquals("There is no unique element in this list.", exception.getMessage());
     }
 
     @Test
     void findSmallestUniqueNullParameterTest() {
-        NumberStat n = new NumberStat();
+        NumberStat n = new NumberStat(null);
 
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> n.findSmallestUnique(null));
+        Exception exception = assertThrows(IllegalArgumentException.class, n::findSmallestUnique);
         assertEquals("The list is a must!", exception.getMessage());
+    }
+
+    @Test
+    void loadFromFileTest() {
+        NumberStat n = new NumberStat(null);
+
+        n.loadFromFile(Path.of("src/main/resources/week12/d03/numbers.txt"));
+
+        assertEquals(List.of(1, 1, 4, 2, 3, 5, 2, 2, 5, 645, 744, 322, 21, 4, 56, 3, -34, -67, 0, -1, -3, 5),
+                n.getList());
+    }
+
+    @Test
+    void loadFromFileInvalidFileNameTest() {
+        NumberStat n = new NumberStat(null);
+
+        Exception exception = assertThrows(IllegalStateException.class, () -> n.loadFromFile(Path.of("")));
+        assertEquals("Can't read from file: ", exception.getMessage());
+    }
+
+    @Test
+    void loadFromFileParameterTest() {
+        NumberStat n = new NumberStat(null);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> n.loadFromFile(null));
+        assertEquals("The file name is a must!", exception.getMessage());
     }
 }
