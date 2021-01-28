5fbd551a60711c3f9be0bbccc627162382b8c979
exylaci <exy@freemail.hu>
2021-01-28 21:43:52 +0100

    week 13 / csütörtök / órai munka: körlevél UpDate: clean code + teszteset
    Írj egy sablonkezelő rendszert! Hozz létre egy TemplateEngine nevezetű osztályt, benne egy
    void merge(BufferedReader reader, Map<String, Object>, BufferedWriter writer)
    metódussal. Az első paraméter a sablon, a második paraméter pedig az értékek, melyeket ki kell cserélni a sablonban.
    Képzeljük el, hogy ez a sablon:
    
    Kedves {nev}!
    Megküldjük önnek a következő esedékes számláját {datum} dátummal, melynek összege: {osszeg} Ft!
    A fizetési határidő {hatarido}.
    
    Üdvözlettel,
    Ügyfélszolgálat
    
    A metódus második paraméterként egy Map-ben kapja az értékeket:
    {"nev" = "John Doe", "datum" = LocalDate}
    
    A feladat, hogy a writer-be ki kell írni a levelet, de már kicserélve az értékeket.

--- src/main/java/week13/d04/TemplateEngine.java
+++ src/main/java/week13/d04/TemplateEngine.java
@@ -3,37 +3,71 @@ package week13.d04;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Map;
 
 public class TemplateEngine {
 
-    public static final String KEY_START = "\\{";
-    public static final String KEY_CLOSE = "\\}";
+    public static final Locale LOCALE = new Locale("hu", "HU");
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy. MMMM d.").withLocale(LOCALE);
+    public static final NumberFormat NUMBER_FORMATTER = DecimalFormat.getCurrencyInstance(LOCALE);
+    public static final String KEY_START = "{";
+    public static final String KEY_CLOSE = "}";
 
     public void merge(BufferedReader reader, Map<String, Object> changes, BufferedWriter writer) {
         String oneLine;
 
         try {
             while ((oneLine = reader.readLine()) != null) {
-                oneLine = replaces(oneLine, changes);
-                writer.write(oneLine);
+                String newLine = replaces(oneLine, changes);
+                writer.write(newLine);
+                writer.newLine();
             }
         } catch (IOException e) {
-            throw new IllegalStateException("Can't read from file!");
+            throw new IllegalStateException("Can't read or write the file!");
         }
-
     }
 
     private String replaces(String oneLine, Map<String, Object> changes) {
-        String[] parts = oneLine.split(KEY_START);
-        if (parts.length == 1) return oneLine;
+        while (oneLine.contains(KEY_START)) {
+            int begin = oneLine.indexOf(KEY_START);
+            int end = oneLine.indexOf(KEY_CLOSE);
 
-        StringBuilder newLine = new StringBuilder(parts[0]);
-        for (int i = 1; i < parts.length; ++i) {
-            String[] subParts = parts[i].split(KEY_CLOSE);
-            newLine.append(changes.get(subParts[0]).toString()).append(subParts[1]);
+            String key = oneLine.substring(begin + 1, end);
+            Object value = changes.get(key);
+
+            String valueString;
+            if (value instanceof LocalDate) {
+                valueString = ((LocalDate) value).format(DATE_FORMATTER);
+            } else if (value instanceof Double || value instanceof Float) {
+                valueString = NUMBER_FORMATTER.format(value);
+            } else {
+                valueString = value.toString();
+            }
+
+            oneLine = oneLine.replace(KEY_START + key + KEY_CLOSE, valueString);
         }
-        return newLine.toString();
+        return oneLine;
     }
-
 }
+//Írj egy sablonkezelő rendszert! Hozz létre egy TemplateEngine nevezetű osztályt, benne egy
+//void merge(BufferedReader reader, Map<String, Object>, BufferedWriter writer)
+//metódussal. Az első paraméter a sablon, a második paraméter pedig az értékek, melyeket ki kell cserélni a sablonban.
+//Képzeljük el, hogy ez a sablon:
+//
+//Kedves {nev}!
+//
+//Megküldjük önnek a következő esedékes számláját {datum} dátummal, melynek összege: {osszeg} Ft!
+//A fizetési határidő {hatarido}.
+//
+//Üdvözlettel,
+//Ügyfélszolgálat
+//
+//A metódus második paraméterként egy Map-ben kapja az értékeket:
+//{"nev" = "John Doe", "datum" = LocalDate}
+//
+//A feladat, hogy a writer-be ki kell írni a levelet, de már kicserélve az értékeket.
--- src/test/java/week13/d04/TemplateEngineTest.java
+++ src/test/java/week13/d04/TemplateEngineTest.java
@@ -2,14 +2,57 @@ package week13.d04;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
+import java.io.*;
+import java.time.LocalDate;
 import java.util.Map;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TemplateEngineTest {
 
     @Test
-    void merge() {
-        Map<String, Object> temp = new HashMap<>();
+    void merge() throws IOException {
+        String template = """
+                Kedves {nev}!
 
+                Megküldjük önnek a következő esedékes számláját {datum} dátummal, melynek összege: {osszeg}!
+                A fizetési határidő {hatarido}.
+
+                Üdvözlettel,
+                Ügyfélszolgálat""";
+
+        Map<String, Object> changes = Map.of(
+                "nev", "John Doe",
+                "datum", LocalDate.of(2021, 01, 28),
+                "osszeg", 30000.99,
+                "hatarido", LocalDate.of(2021, 3, 17));
+
+        StringReader source = new StringReader(template);
+        BufferedReader reader = new BufferedReader(source);
+
+        StringWriter result = new StringWriter();
+        BufferedWriter writer = new BufferedWriter(result);
+
+        new TemplateEngine().merge(reader, changes, writer);
+
+        writer.flush();
+        result.flush();
+
+        String expected = """
+                Kedves John Doe!
+                                
+                Megküldjük önnek a következő esedékes számláját 2021. január 28. dátummal, melynek összege: 30 000,99 Ft!
+                A fizetési határidő 2021. március 17..
+                                
+                Üdvözlettel,
+                Ügyfélszolgálat
+                """;
+
+        Scanner original = new Scanner(expected);
+        Scanner outcome = new Scanner(result.toString());
+        while (original.hasNext()) {
+            assertEquals(original.nextLine(), outcome.nextLine());
+        }
     }
 }
