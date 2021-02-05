162083264524dcce778f5bc35f15875b9b8ca335
exylaci <exy@freemail.hu>
2021-02-05 15:55:43 +0100

    week 14 / péntek / órai munka: stream read from file and count
    A examples/week14d05/hachiko.srt van a Hachiko film felirata. Írj egy
    countWords() metódust, mely paraméterül várja a fájlt, varargsban
    szavakat. Megszámolja, hogy melyik szó hány sorban szerepel a
    szövegben. Legyenek mondjuk a beadott szavak: "Hachiko", "haza",
    "pályaudvar", "jó"## SeniorA examples/week14d05/hachiko.srt van a Hachiko film felirata. Csak streamek
    használatával számold ki, hogy hányszor hangzik el a kutya neve a filmben!Segítség: a Files.lines() adja vissza a fájl sorait streamben.

--- src/main/java/week14/d05/WordCounter.java
+++ src/main/java/week14/d05/WordCounter.java
@@ -4,13 +4,31 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class WordCounter {
-    public int countWords(Path parh) {
+    public Map<String , Long> countWords(Path path, String[] words){
+        List<String> wordsInList = Arrays.asList(words);
+        try {
+            return Files
+                    .lines(path, StandardCharsets.UTF_8)
+                    .flatMap(line -> Arrays.stream(line.split(" ").clone()))
+                    .filter(word->wordsInList.contains(word))
+                    .collect( Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't read the file!");
+        }
+    }
+
+    public int countWord(Path parh) {
         try {
             return (int) Files
                     .lines(parh, StandardCharsets.UTF_8)
-                    .filter(oneLine -> oneLine.contains("Hachiko")||oneLine.contains("Hachi"))
+                    .filter(oneLine -> oneLine.contains("Hachiko"))
                     .count();
         } catch (IOException e) {
             throw new IllegalStateException("Can't read the file!");
--- src/test/java/week14/d05/WordCounterTest.java
+++ src/test/java/week14/d05/WordCounterTest.java
@@ -9,7 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class WordCounterTest {
 
     @Test
+    void countWordTest() {
+        assertEquals(83, new WordCounter().countWord(Path.of("src/main/resources/week14/d05/hachiko.srt")));
+    }
+
+    @Test
     void countWordsTest() {
-        assertEquals(83, new WordCounter().countWords(Path.of("src/main/resources/week14/d05/hachiko.srt")));
+        WordCounter w = new WordCounter();
+        System.out.println(w.countWords(Path.of("src/main/resources/week14/d05/hachiko.srt"),
+           new String[]{"Hachiko","haza","pályaudvar","jó"}     ));
+
     }
 }
