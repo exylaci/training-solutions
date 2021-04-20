9f94819987c6c98ca7b0baab39a114c64c7793bf
exylaci <exy@freemail.hu>
2021-04-21 00:06:33 +0200

    Gyakorló feladatok Kovács Enikőtől #6 Szómátrix streammel

--- src/main/java/gyaxi/kovacseni/finelongword/FineLongWord.java
+++ src/main/java/gyaxi/kovacseni/finelongword/FineLongWord.java
@@ -5,27 +5,49 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class FineLongWord {
     public static final String PATH = "src/main/resources/gyaxi/kovacseni/finelongword";
     public static final String ERROR = "Can't read from file!";
 
+//    public char[] readFineLongWordFromFileAndGetItInAnArray(String fileName) {
+//
+//        try (BufferedReader reader = Files.newBufferedReader(Path.of(PATH).resolve(fileName), StandardCharsets.UTF_8)) {
+//
+//            StringBuilder sb = new StringBuilder();
+//            sb.append(reader.readLine());
+//
+//            String oneLine;
+//            while ((oneLine = reader.readLine()) != null) {
+//                sb.append(oneLine.charAt(oneLine.length() - 1));
+//            }
+//
+//            return sb.toString().toCharArray();
+//
+//        } catch (IOException e) {
+//            throw new IllegalStateException(ERROR, e);
+//        }
+//    }
+
     public char[] readFineLongWordFromFileAndGetItInAnArray(String fileName) {
 
-        try (BufferedReader reader = Files.newBufferedReader(Path.of(PATH).resolve(fileName), StandardCharsets.UTF_8)) {
+        try (Stream<String> streamFirstLine = Files.lines(Path.of(PATH).resolve(fileName), StandardCharsets.UTF_8);
+             Stream<String> streamRestLines = Files.lines(Path.of(PATH).resolve(fileName), StandardCharsets.UTF_8)) {
 
-            StringBuilder sb = new StringBuilder();
-            sb.append(reader.readLine());
-
-            String oneLine;
-            while ((oneLine = reader.readLine()) != null) {
-                sb.append(oneLine.charAt(oneLine.length() - 1));
-            }
-
-            return sb.toString().toCharArray();
+            return streamRestLines
+                    .skip(1)
+                    .reduce(streamFirstLine.findFirst().orElse(""),
+                            (String total, String oneLine) -> total += getLastCharacter(oneLine),
+                            String::concat)
+                    .toCharArray();
 
         } catch (IOException e) {
             throw new IllegalStateException(ERROR, e);
         }
+    }
+
+    private char getLastCharacter(String oneLine) {
+        return oneLine.charAt(oneLine.length() - 1);
     }
 }
