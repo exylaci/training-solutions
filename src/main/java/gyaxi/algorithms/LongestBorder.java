b2ff24c46568c0044d7a4aedc2ebb125224ee3f1
exylaci <exy@freemail.hu>
2021-05-14 20:15:19 +0200

    Find the longest border Fájlból felolvassa a teszt adatot.
    https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/maximum-border-9767e14c/

--- src/main/java/gyaxi/algorithms/LongestBorder.java
+++ src/main/java/gyaxi/algorithms/LongestBorder.java
@@ -57,6 +57,8 @@ public class LongestBorder {
         return longest;
     }
 }
+//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/maximum-border-9767e14c/
+
 //You are given a table with  rows and columns. Each cell is colored with white or black. Considering the shapes created by black cells, what is the maximum border of these shapes?
 //A shape is a set of connected cells. Two cells are connected if they share an edge. Note that no shape has a hole in it."
 //Input:
--- src/test/java/gyaxi/algorithms/LongestBorderTest.java
+++ src/test/java/gyaxi/algorithms/LongestBorderTest.java
@@ -2,6 +2,11 @@ package gyaxi.algorithms;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LongestBorderTest {
@@ -41,5 +46,30 @@ class LongestBorderTest {
         char[][] shape = new char[][]{{'#'}};
 
         assertEquals(1, new LongestBorder().longestBorder(shape));
+    }
+
+    @Test
+    void longestBorderFromFile() throws IOException {
+        int[] results = new int[]{4,5,9,6,5,7,2,1,11,5};
+
+        BufferedReader reader = Files.newBufferedReader(
+                Path.of("src/test/resources/gyaxi/algorithms/LongestBorder/shapes.txt"));
+
+        int pieces = Integer.parseInt(reader.readLine());
+
+        for (int piece = 0; piece < pieces; ++piece) {
+            String[] parts = reader.readLine().split(" ");
+            int rows = Integer.parseInt(parts[0]);
+            int columns = Integer.parseInt(parts[1]);
+
+            char[][] shape = new char[rows][columns];
+            for (int row = 0; row < rows; ++row) {
+                String oneLine=reader.readLine();
+                for(int column=0;column<columns;++column){
+                    shape[row][column]=oneLine.charAt(column);
+                }
+            }
+            assertEquals(results[piece], new LongestBorder().longestBorder(shape));
+        }
     }
 }
